f52fe5eefc402c9b51bb62e515db29d892f0138a
BikashSantra03 <santrabikash922@gmail.com>
2025-02-18 11:24:55 +0530

    Episode 2 code commmited

--- app.js
+++ app.js
@@ -1,25 +1,5 @@
-// const heading = React.createElement(
-//   "h1",
-//   {
-//     id: "heading",
-//   },
-//   "Hlw from react!"
-// );
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-
-// root.render(heading);
-
-// ---------------------------------------------------------------------
-
-// const parent = React.createElement(
-//   "div",
-//   { id: "parent" },
-//   React.createElement(
-//     "div",
-//     { id: "child" },
-//     React.createElement("h1", {}, "This is the h1")
-//   )
-// );
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement(
   "div",
