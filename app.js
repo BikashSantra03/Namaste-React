fcbde22ebec3d326ac52c7fd05d52923140b3d68
BikashSantra03 <santrabikash922@gmail.com>
2025-02-18 18:02:29 +0530

    React Functional Component Concept Episode 3

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//JSX => Babel transpiles it to React.createElement() => Reactelement - object => HTML element (render)
+const para = <p id="para">Hi I am Biaksh</p>; // React Element
 
-const jsxheading = <h1 id="heading">This is namaste react</h1>;
-console.log(jsxheading); //React element=> object
+const Title = () => <p>This is a Title para</p>; // React Component
+
+const number = 3; // normal variable
+
+//Component composition => one component inside another
+const HeadingComponent = () => (
+  <div className="container">
+    <h1 id="heading">Namaste React Functional Component Episode - {number}</h1>
+
+    <h2>{console.log("just loging")}</h2>
+
+    <Title />
+
+    <Title></Title>
+
+    {Title()}
+
+    {para}
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxheading);
+root.render(<HeadingComponent />);
