b4d6efb93dd3aa758fe33f9d94a85a84ae69eb83
BikashSantra03 <santrabikash922@gmail.com>
2025-02-19 15:17:13 +0530

    React Hook - UseState , Episode 5 code

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,7 +1,15 @@
+import { useState } from "react";
 import { resList } from "../utils/mockData";
 import RestaurantCard from "./RestaurentCard";
 
 const Body = () => {
+  // * React Hook -> A normal JavaScript function which is given to us by React (or) Normal JS utility functions
+  // * useState() - Super Powerful variable
+  // * useEffect() -
+
+  // * State variable - Super Powerful variable
+  const [listOfRestraunt, setListOfRestraunt] = useState(resList);
+
   return (
     <div className="body">
       <div className="search-container">
@@ -10,13 +18,19 @@ const Body = () => {
           type="text"
           placeholder="Search Food or Restaurant"
         />
-        <button>
+        <button
+          onClick={() => {
+            setListOfRestraunt(
+              listOfRestraunt.filter((res) => res.data.avgRating > 4)
+            );
+          }}
+        >
           <label htmlFor="search">Search</label>
         </button>
       </div>
 
       <div className="cards-container">
-        {resList.map((items) => (
+        {listOfRestraunt.map((items) => (
           <RestaurantCard key={items?.data.id} resData={items} />
         ))}
       </div>
